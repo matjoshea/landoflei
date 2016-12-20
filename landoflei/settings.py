4edb90021d112f24e736cdb2cf1af1debb2babf3
Matthew Barcus <mbarcus@gmail.com>
2016-12-19 22:36:26 -0500

    updated ebextensions config files

--- landoflei/settings.py
+++ landoflei/settings.py
@@ -78,12 +78,26 @@ WSGI_APPLICATION = 'landoflei.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.environ['RDS_DB_NAME'],
+            "USER": os.environ['RDS_USERNAME'],
+            "PASSWORD": os.environ['RDS_PASSWORD'],
+            "HOST": os.environ['RDS_HOSTNAME'],
+            "PORT": os.environ['OS.PORT'],
+        }}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "lol",
+            "USER": "",
+            "PASSWORD": "",
+            "HOST": "",
+            "PORT": "",
+        }}
 
 
 # Password validation
