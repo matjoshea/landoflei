91ac7ef3f52a701526d3c7fb16f6697628202446
Matthew Barcus <mbarcus@gmail.com>
2016-12-19 23:38:27 -0500

    sqlite settings

--- landoflei/settings.py
+++ landoflei/settings.py
@@ -78,27 +78,33 @@ WSGI_APPLICATION = 'landoflei.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.environ['RDS_DB_NAME'],
-            "USER": os.environ['RDS_USERNAME'],
-            "PASSWORD": os.environ['RDS_PASSWORD'],
-            "HOST": os.environ['RDS_HOSTNAME'],
-            "PORT": os.environ['OS.PORT'],
-        }}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": "lol",
-            "USER": "",
-            "PASSWORD": "",
-            "HOST": "",
-            "PORT": "",
-        }}
+    # if 'RDS_DB_NAME' in os.environ:
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #         "NAME": os.environ['RDS_DB_NAME'],
+    #         "USER": os.environ['RDS_USERNAME'],
+    #         "PASSWORD": os.environ['RDS_PASSWORD'],
+    #         "HOST": os.environ['RDS_HOSTNAME'],
+    #         "PORT": os.environ['OS.PORT'],
+    #     }}
+    # else:
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #         "NAME": "lol",
+    #         "USER": "",
+    #         "PASSWORD": "",
+    #         "HOST": "",
+    #         "PORT": "",
+    #     }}
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
